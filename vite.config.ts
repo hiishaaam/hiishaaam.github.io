c741363d0ea18dc88a843d58791244a962a1de26
hiishaaam <hishamaju189@gmail.com>
2026-04-05 17:01:06 +0530

    Configure base route for Pages

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,7 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
+  base: '/Personal-Portfolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
