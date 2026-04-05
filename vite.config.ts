74dfeeb628ec58479b709a835cfb2fde295c2d22
hiishaaam <hishamaju189@gmail.com>
2026-04-05 17:03:28 +0530

    Update base path for user website root

--- vite.config.ts
+++ vite.config.ts
@@ -4,7 +4,6 @@ import path from 'path';
 import {defineConfig} from 'vite';
 
 export default defineConfig({
-  base: '/Personal-Portfolio/',
   plugins: [react(), tailwindcss()],
   resolve: {
     alias: {
